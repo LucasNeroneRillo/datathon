46e37df784371f433b81ac08a8679dee8a1e02dc
Lucas Nerone Rillo <lucas@lucasnr.student.iastate.edu>
2023-10-21 19:51:09 -0500

    Group by hour/detector

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 import re
 import pandas as pd
 
@@ -7,50 +8,33 @@ from globals import *
 
 
 def main():
+    # Open file
     filename = "Wavetronix Nov Dev 2023.csv" if is_big_dataset() else "one_sensor.csv"
     df = pd.read_csv(FILE_ROOT + filename)
+    
+    # Drop unnecessary columns
     columns_to_remove = ['owner-id', 'network-id', 'date', 'utc-offset', 'start-ime', 'end-time', 'small-class-count', 'medium-class-count', 'large-class-count', 'device-id', 'detector-type', 'cst-time']
-
     df = df.drop(columns=columns_to_remove)
     
-
+    # Clean data
     df = df[df['status'] == 'OK']
     df = df[df['device-description'] == 'NORMAL']
     df = df[df['time'] >= 80000]
     df = df[df['time'] <= 235900]
-    # indexAge = df[ (df['status'] == 'OK') | (df['device-description'] == 'NORMAL') ].index
-    # df.drop(indexAge , inplace=True)
+    df['month-day-hour'] = df['month'].astype(str) + '-' + df['day'].astype(str) + '-' + df['time'].astype(str).str[:2]
+    df['detector-id'] = df['detector-id'].apply(lambda x: int(re.search(r'\d+', x).group())).astype(str) + df['link-direction']
 
-    # print(df['detector-id'])
-    # print(df['detector-id'].dtype)
-    print(df.info())
-    print(df.shape[0])
+    # Group by hour/detector
+    df = df.groupby(['month-day-hour', 'detector-id']).agg({
+        'lane-count': 'sum',
+        'lane-occupancy': lambda x: np.average(x, weights=df.loc[x.index, 'sample-period']),
+        'lane-speed': lambda x: np.average(x, weights=df.loc[x.index, 'lane-count']),
+    }).reset_index()
 
+    #print(df)
+    #print(df.info())
+    #print(df.shape[0])
 
-# Function to update detector-id to only return 19
-def update_detector_id(row):
-    for text_value in row['detector-id']:
-        return int(re.search(r'\d+', row['detector-id']).group())
-
-
-# Function to make where the OK is not found in Status and make it an empty row
-def update_status(row):
-    target = 'OK'
-    for text_value in row['status']:
-        if target in row['status']:
-            return row['status']
-        else:
-            
-            return None
-        
-# Function to ensure device-description is NORMAL
-def update_device_description(row):
-    target = 'NORMAL'
-    #/for text_value in row['device-description']:
-    if target in row['device-description']:
-        return row['device-description']
-    else:
-        return None
 
 
 main()
