640a03108004075bef4fa5d985176a734005eb78
Lucas Nerone Rillo <lucas@lucasnr.student.iastate.edu>
2023-10-21 20:46:56 -0500

    Simple example

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
-import matplotlib
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 from globals import *
 from utils import *
@@ -9,5 +10,8 @@ def main():
     filename = "cleaned_data.csv"
     df = pd.read_csv(FILE_ROOT + filename)
 
-    
-    
+    df.plot(kind='scatter', x="lane-speed", y="lane-occupancy")
+
+    plt.show()
+
+main()
