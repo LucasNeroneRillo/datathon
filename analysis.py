7e053f2ed9555fd74e9dd9b298931a3dd45e0a6a
Lucas Nerone Rillo <lucas@lucasnr.student.iastate.edu>
2023-10-21 20:49:01 -0500

    Simple example

--- analysis.py
+++ analysis.py
@@ -9,6 +9,9 @@ def main():
     # Open file
     filename = "cleaned_data.csv"
     df = pd.read_csv(FILE_ROOT + filename)
+    df['month'] = df['month-day-hour'].apply(lambda x: x.split('-')[0])
+    df['day'] = df['month-day-hour'].apply(lambda x: x.split('-')[1])
+    df['hour'] = df['month-day-hour'].apply(lambda x: x.split('-')[2])
 
     df.plot(kind='scatter', x="lane-speed", y="lane-occupancy")
 
